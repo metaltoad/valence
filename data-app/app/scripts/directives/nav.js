618240fa85fda8b633b4c1331c21123c085ffc1f
Ken Stowell <ken@metaltoad.com>
2014-05-31 14:19:08 -0700

    Updating nav to use new valence auth/acl API.

--- data-app/app/scripts/directives/nav.js
+++ data-app/app/scripts/directives/nav.js
@@ -3,22 +3,20 @@ app.directive('datanav', function($compile) {
     scope: false,
     restrict: 'E',
     templateUrl: '/scripts/directives/templates/nav.html',
-    controller: function($scope, $element, $attrs, auth, $rootElement, $rootScope, model) {
+    controller: function($scope, $element, $attrs, $rootElement, $rootScope, valence) {
 
       $scope.getNavTemplate = function() {
-        return 'http://localhost:9000/scripts/directives/templates/' + ((auth.isValidated)? 'logged-in_nav.html' : 'logged-out_nav.html');
+        return 'http://localhost:9000/scripts/directives/templates/' + ((valence.auth.isValidated)? 'logged-in_nav.html' : 'logged-out_nav.html');
       };
 
       // This catched page load
-      auth.validate().then(function(data) {
-        model.get('user').then(function(user) {
-          $rootScope.currentUser = user;
-        });
+      valence.auth.validate().then(function(identity) {
+        $rootScope.identity = identity;
       });
 
       $scope.logout = function() {
-        auth.logout().then(function(data) {
-          $scope.user = {};
+        valence.auth.logout().then(function(identity) {
+          $rootScope.identity = identity;
         });
       }
     }
