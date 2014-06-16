2e61923c8733f59caa330fdd7d8b782f8a3260bc
Ken Stowell <kstowell@sosacopr.com>
2014-06-15 20:27:34 -0700

    Removing the strategy args mutation from a failed promise to preserve argument state when a fail occurs.

--- data-app/app/bin/valence-model.js
+++ data-app/app/bin/valence-model.js
@@ -362,10 +362,6 @@ valenceApp.service('model', ['valence', 'cloud', 'store', 'loader', 'auth', '$ro
             }
           }
         }, function(data) {
-
-          if(data) {
-            self.args.data = data
-          }
           
           if(strategy.fail) {
             if(strategy.fail.constructor === Function) {
