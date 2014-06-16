c221e9f49aea9c7cf25ceb86c70c93e3bacd4225
Ken Stowell <kstowell@sosacopr.com>
2014-06-15 23:42:12 -0700

    Prior fix for minification break somehow got lost. Reverting now.

--- data-app/app/bin/valence-model.js
+++ data-app/app/bin/valence-model.js
@@ -531,7 +531,8 @@ valenceApp.service('model', ['valence', 'cloud', 'store', 'loader', 'auth', '$ro
    * @description [description]
    */
   function apply(args) {
-    var scope;
+    var scope,
+        data;
     
     // Detect a scope set.
     for(var i=0; i<valence.models.length; i++) {
